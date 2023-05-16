a4a83168e45f88996ebf96a411466d039849444b
Lucas Pedroso <lucaspedroso@brq.com>
2023-05-16 18:04:18 -0300

    implementing alamofire adapter in repository with dependency injection

--- CoinAPI/Data/Repositories/HomeRepository.swift
+++ CoinAPI/Data/Repositories/HomeRepository.swift
@@ -1,10 +1,26 @@
+import Foundation
+
 protocol HomeRepositoryProtocol {
     func getExchanges(completion: @escaping (Result<ExchangesEntity, Error>) -> Void)
 }
 
 class HomeRepository: HomeRepositoryProtocol {
 
+    let network: HttpGetClient
+    
+    init(network: HttpGetClient) {
+        self.network = network
+    }
+    
     func getExchanges(completion: @escaping (Result<ExchangesEntity, Error>) -> Void) {
-        //call alamofire
+        guard let url = URL(string: "") else { return }
+        network.get(to: url) { [weak self] result in
+            switch result {
+            case .failure:
+                break
+            case .success:
+                break
+            }
+        }
     }
 }
