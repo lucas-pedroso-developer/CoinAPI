f2d4d5f8fc1e5fbf1d93c8b8cde3dcdaee39e3ac
Lucas Pedroso <lucaspedroso@brq.com>
2023-05-21 18:31:22 -0300

    adjusting tableview selection style

--- CoinAPI/Presentation/Views/ViewControllers/HomeViewController.swift
+++ CoinAPI/Presentation/Views/ViewControllers/HomeViewController.swift
@@ -85,6 +85,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cell.rawValue, for: indexPath) as? ExchangesCell else { return UITableViewCell() }
+        cell.selectionStyle = .none
         cell.setupCell(
             title: viewModel?.getExchangeId(index: indexPath.row) ?? String(),
             subtitle: viewModel?.getExchangeName(index: indexPath.row) ?? String(),
